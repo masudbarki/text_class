7de27d945c720de878b199f730b6eea1feb1218c
masudbarki <61346065+masudbaki@users.noreply.github.com>
2021-02-25 14:28:23 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,3 +3,12 @@ app= Flask(__name__)
 @app.route('/')
 def index():
   return "<h1>Welcome to CodingX</h1>"
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    # get data
+    if request.method == 'POST':
+		    message = request.form['message']
+		    data = [message]
+    return message
